5570ef1a4eed8a95b6685b27363c44bfa9cccf2d
Paul <wi22b104@technikum-wien.at>
2024-09-27 20:18:55 +0200

    added navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,91 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-custom-darkblue pl-1 pr-1">
+      <router-link to="/" class="navbar-brand">
+        <img src="../assets/basseno.png" class="bassenoBild" />
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <input type="text" class="form-control" id="floatingInput" placeholder="Suche" />
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/produkte">Produkte</router-link>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/login">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/register">Registrierung</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link position-relative" to="/cart" data-bs-toggle="tooltip" title="Einkaufswagen">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+              </svg>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NavBar',
+  };
+  </script>
+  
+  <style scoped>
+  .navbar {
+    background: linear-gradient(to bottom, lightblue, ghostwhite);
+    padding: 10px;
+  }
+  
+  .bassenoBild {
+    width: 200px;
+    margin-right: 15px;
+  }
+  
+  .nav-link {
+    color: black;
+    font-size: 16px;
+    text-decoration: none;
+  }
+  
+  .nav-item.dropdown .dropdown-menu {
+    top: 100%;
+    transform: translateY(0);
+  }
+  
+  .custom-translate {
+    transform: translate(-80%, -20%);
+  }
+  
+  #cart-count {
+    width: 20px;
+    height: 20px;
+    padding: 4px;
+    color: white;
+    background-color: red;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 12px;
+    font-weight: bold;
+  }
+  </style>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import LoginView from '../views/LoginView.vue';
+import RegisterView from '../views/RegisterView.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import(/* webpackChunkName: "login" */ '../views/LoginView.vue')
-  },
-  {
-    path: '/register',
-    name: 'register',
-    component: () => import(/* webpackChunkName: "register" */ '../views/RegisterView.vue')
-  }
-]
+  { path: '/', name: 'Home', component: HomeView },
+  { path: '/login', name: 'Login', component: LoginView },
+  { path: '/register', name: 'Register', component: RegisterView },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
