312e40081db2ca53e1934652e56252676d59b5c1
Paul <wi22b104@technikum-wien.at>
2024-11-07 23:46:21 +0100

    project update

--- bweng/src/main/java/at/technikum/springrestbackend/SpringRestBackendApplication.java
+++ bweng/src/main/java/at/technikum/springrestbackend/SpringRestBackendApplication.java
@@ -1,13 +1,13 @@
-package at.uastw.webshopbackend;
+package at.technikum.springrestbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class SpringRestBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication.run(SpringRestBackendApplication.class, args);
     }
 
 }
