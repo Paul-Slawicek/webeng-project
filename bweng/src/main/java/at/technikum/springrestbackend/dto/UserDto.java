58ee0b94b0ee7ffa3d9ce3c6f7970709d9f29e5e
Paul <wi22b104@technikum-wien.at>
2024-11-10 01:57:20 +0100

    created user dto

---
+++ bweng/src/main/java/at/technikum/springrestbackend/dto/UserDto.java
@@ -0,0 +1,26 @@
+package at.technikum.springrestbackend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserDto(
+        Long id,
+        @NotBlank(message = "Username is required")
+        @Size(min = 5, message = "Username must be at least 5 characters")
+        String username,
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email should be valid")
+        String email,
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, message = "Password must be at least 8 characters")
+        String password,
+        String role,
+        String firstname,
+        String lastname,
+        String address,
+        String city,
+        String plz,
+        String salutation
+) {
+}
--- bweng/src/main/java/at/technikum/springrestbackend/entity/User.java
+++ bweng/src/main/java/at/technikum/springrestbackend/entity/User.java
@@ -15,18 +15,12 @@ public class User {
     private Long id;
 
     @Column(nullable = false, unique = true)
-    @NotBlank(message = "Username is required")
-    @Size(min = 5, message = "Username must be at least 5 characters")
     private String username;
 
     @Column(nullable = false, unique = true)
-    @NotBlank(message = "Email is required")
-    @Email(message = "Email should be valid")
     private String email;
 
     @Column(nullable = false)
-    @NotBlank(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 
     private String role = "user";
