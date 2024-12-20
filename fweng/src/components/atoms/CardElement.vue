70750897c9253a2c6b52e810691bb7822593877d
Enes <enesozturkk10@gmail.com>
2024-12-20 20:49:23 +0100

    Atomic Index page

---
+++ fweng/src/components/atoms/CardElement.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card" @click="$emit('cardClick')">
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "CardAtom",
+    emits: ["cardClick"], // Emit-Event für Klick-Interaktionen
+  };
+  </script>
+  
+  <style scoped>
+  .card {
+    border: 1px solid #ddd;
+    border-radius: 8px;
+    overflow: hidden;
+    transition: transform 0.3s ease, box-shadow 0.3s ease;
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+  }
+  .card:hover {
+    transform: scale(1.05);
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+  }
+  </style>
+  
